999b1126ac2847ae40de1a562844622f8d8e9d79
Yuns Xie <yuns.xie@qq.com>
2019-11-05 18:27:04 +0800

    refactor: to Editor

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import router from 'umi/router';
 import Editor from 'gaea-editor';
 // import BasicComponents from 'gaea-basic-components';
 import { Div, SimpleCard } from '@/gaea-components/container';
 import { ImagesCarousel } from '@/gaea-components/data-container';
+import { Descriptions } from '@/gaea-components/display';
 import { WhiteSpace, Button, Grid } from '@/gaea-components/antd-mobile';
 
 export default function Welcome() {
@@ -11,6 +13,7 @@ export default function Welcome() {
       <Editor
         componentClasses={[
           Button,
+          Descriptions,
           Div,
           Grid,
           ImagesCarousel,
@@ -19,6 +22,10 @@ export default function Welcome() {
         ]}
         onSave={(value) => {
           console.log(value);
+          router.push({
+            pathname: '/render',
+            state: value,
+          });
         }}
       />
     </div>
