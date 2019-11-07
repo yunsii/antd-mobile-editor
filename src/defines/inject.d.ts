8afa21e1e832e98eae3b50c2e6f1829ddcdc326a
Yuns Xie <yuns.xie@qq.com>
2019-11-07 16:12:55 +0800

    feat: InjectProps

---
+++ src/defines/inject.d.ts
@@ -0,0 +1,14 @@
+import { DescriptionsData } from "@/gaea-components/display/Descriptions/type";
+import { GirdData } from "@/gaea-components/antd-mobile/Grid/type";
+import { ImagesCarouselData } from "@/gaea-components/data-container/ImagesCarousel/type";
+
+declare type InjectPropsValue =
+  DescriptionsData |
+  GirdData |
+  ImagesCarouselData |
+  boolean |  // loading and etc
+  Function   // event callback
+
+declare interface InjectProps {
+  [k: string]: InjectPropsValue;
+}
