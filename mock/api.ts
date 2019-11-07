c239a805111dfb2c2c5c1cb68dd993fd0b2005bd
Yuns Xie <yuns.xie@qq.com>
2019-11-07 23:04:57 +0800

    feat: /gaea-jsons, /get/gaea-json/:name and /save/gaea-json/:name

--- mock/api.ts
+++ mock/api.ts
@@ -1,18 +1,63 @@
-import { delay } from 'roadhog-api-doc';
+import fs from 'fs';
 
-export default delay({
-  'POST /api/login/account': (req, res) => {
-    const { password, username } = req.body;
-    if (password === 'password' && username === 'admin') {
-      res.send({
-        status: 'ok',
-        currentAuthority: 'admin',
-      });
-      return;
-    }
-    res.send({
-      status: 'error',
-      currentAuthority: 'guest',
+const relativeJsonsPath = '/src/assets/gaea-json';
+
+export default {
+  'GET /gaea-jsons': (req, res) => {
+    fs.readdir(`${process.cwd()}${relativeJsonsPath}`, (err, files) => {
+      if (err) {
+        res.send({
+          status: 'error',
+          message: err.message,
+        })
+      } else {
+        const result: string[] = [];
+        files.forEach((file) => {
+          console.log(file);
+          result.push(file);
+        });
+        res.send({
+          status: 'ok',
+          data: result,
+        })
+      }
     });
   },
-}, 1200);
+
+  'GET /get/gaea-json/:name': (req, res) => {
+    const { name } = req.params;
+    const relativePath = `${relativeJsonsPath}/${name}`;
+    const readPath = `${process.cwd()}${relativePath}`.replace(/\\/g, '/');
+    try {
+      const renderJson = fs.readFileSync(readPath, { encoding: 'utf-8' });
+      res.send({
+        status: 'ok',
+        data: JSON.parse(renderJson),
+      });
+    } catch (err) {
+      res.send({
+        status: 'error',
+        message: err.message,
+      });
+    }
+  },
+
+  'POST /save/gaea-json/:name': (req, res) => {
+    const { name }: { name: string } = req.params;
+    const { renderJson } = req.body;
+    const relativePath = `${relativeJsonsPath}/${name}${name.endsWith('.json') ? '' : '.json'}`;
+    const savePath = `${process.cwd()}${relativePath}`.replace(/\\/g, '/');
+    try {
+      fs.writeFileSync(savePath, `${JSON.stringify(renderJson, null, 2)}\n`);
+      res.send({
+        status: 'ok',
+        data: relativePath,
+      });
+    } catch (err) {
+      res.send({
+        status: 'error',
+        message: err.message,
+      });
+    }
+  },
+};
