65bf64eb63065d6e7f653b19d3747d3ff155afde
Yuns Xie <yuns.xie@qq.com>
2019-11-07 16:14:39 +0800

    chore: no impact

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -7,7 +7,7 @@ import { ImagesCarousel } from '@/gaea-components/data-container';
 import { Descriptions } from '@/gaea-components/display';
 import { WhiteSpace, Button, Grid } from '@/gaea-components/antd-mobile';
 
-export default function Welcome() {
+export default () => {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Editor
