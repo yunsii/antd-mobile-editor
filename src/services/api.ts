8e2ce97c8508b6c2a5f0ed76decb5fd19ebd0205
Yuns Xie <yuns.xie@qq.com>
2019-11-11 12:09:49 +0800

    chore: remove useless

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-// import { stringify } from 'qs';
-import request from '@/utils/request';
-
-export async function fakeAccountLogin(params) {
-  return request('/api/login/account', {
-    method: 'POST',
-    data: params,
-  });
-}
