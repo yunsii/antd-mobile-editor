980c9d06937595e3c25a2dd34c13d3e78571b59b
Yuns Xie <yuns.xie@qq.com>
2019-11-14 12:06:34 +0800

    chore: add type

--- src/defines/inject.d.ts
+++ src/defines/inject.d.ts
@@ -1,13 +1,16 @@
 import { DescriptionsData } from "@/gaea-components/display/Descriptions/type";
-import { GirdData } from "@/gaea-components/antd-mobile/Grid/type";
+import { GirdData, HandleGridClick } from "@/gaea-components/antd-mobile/Grid/type";
 import { ImagesCarouselData } from "@/gaea-components/data-container/ImagesCarousel/type";
+import { SetFormItems, HandleSubmit } from "@/gaea-components/data-container/Form/type";
 
 declare type InjectPropsValue =
-  DescriptionsData |
-  GirdData |
-  ImagesCarouselData |
-  boolean |  // loading and etc
-  Function   // event callback
+  | DescriptionsData
+  | GirdData
+  | HandleGridClick
+  | ImagesCarouselData
+  | SetFormItems
+  | HandleSubmit
+  | boolean  // loading and etc
 
 declare interface InjectProps {
   [k: string]: InjectPropsValue;
