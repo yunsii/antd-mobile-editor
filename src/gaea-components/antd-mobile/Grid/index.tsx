793c16a602e29b93e3286ec9cf0e0edacf2654f8
Yuns Xie <yuns.xie@qq.com>
2019-11-14 10:42:56 +0800

    fix: unselectable grid if change data to empty
    wraped by div

--- src/gaea-components/antd-mobile/Grid/index.tsx
+++ src/gaea-components/antd-mobile/Grid/index.tsx
@@ -9,17 +9,19 @@ export default class extends React.Component<Props, State> {
 
   public render() {
     return (
-      <DataLoading loading={this.props.loading} data={this.props.data}>
-        <Grid
-          data={this.props.data}
-          columnNum={this.props.columnNum}
-          hasLine={this.props.hasLine}
-          isCarousel={this.props.isCarousel}
-          carouselMaxRow={this.props.carouselMaxRow}
-          square={this.props.square}
-          onClick={this.props.onClick}
-        />
-      </DataLoading>
+      <div>
+        <DataLoading loading={this.props.loading} data={this.props.data}>
+          <Grid
+            data={this.props.data}
+            columnNum={this.props.columnNum}
+            hasLine={this.props.hasLine}
+            isCarousel={this.props.isCarousel}
+            carouselMaxRow={this.props.carouselMaxRow}
+            square={this.props.square}
+            onClick={this.props.onClick}
+          />
+        </DataLoading>
+      </div>
     );
   }
 }
